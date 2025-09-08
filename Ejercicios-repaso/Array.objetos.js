e8468e0a143e29e9ae562dce97eb99caa8aba7e5
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-08 12:02:43 -0300

    Ejercicios de repaso

---
+++ Ejercicios-repaso/Array.objetos.js
@@ -0,0 +1,19 @@
+
+/**
+ * Ejercicio 3 
+ *  Array  y objetos 
+ *Array : Lista de cosas 
+ */
+ let frutas = ["Manzana", "Banana","Naranja"];
+ console.log(frutas[1]);
+
+
+ /**
+  * Objetos 
+  * Objeto : Datos organizados en pares clave-valor 
+  */
+ let persona = {
+    nombre : "Elias",
+    edad : 22
+ }
+ console.log(persona.edad);
