bdc3089e3dc08a1c1d70a68e397f31cab6d12aca
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-08 11:54:59 -0300

    Ejercicios de repaso

---
+++ Ejercicios-repaso/Funciones.js
@@ -0,0 +1,10 @@
+
+/**Ejercicio 2
+ * Funciones 
+ *  
+ * Usar una funcion para saludar a un usuario 
+ */
+function saludar(nombre){
+    return `Hola ${nombre} !`;
+}
+console.log(saludar("Elias"));
---
+++ Ejercicios-repaso/Variablesycondicionales.js
@@ -0,0 +1,13 @@
+
+// Ejercicio 1 
+/* Hacer un programa que procese si es mayor o menor de edad 
+ */
+
+let nombre = "Juan ";
+let edad = 19;
+ if (edad >= 18 ) {
+    console.log(nombre ,":" ,"Eres mayor de edad !");
+ } else {
+    console.log( nombre ,":" , "Eres menor de edad !");
+
+ }
