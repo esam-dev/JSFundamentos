03bcd403313447db5880bed12dfffa9129e1b997
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-08 12:23:14 -0300

    Recorrido de un array

--- Ejercicios-repaso/Array.objetos.js
+++ Ejercicios-repaso/Array.objetos.js
@@ -5,9 +5,12 @@
  *Array : Lista de cosas 
  */
  let frutas = ["Manzana", "Banana","Naranja"];
- console.log(frutas[1]);
+ console.log("El primer elemento del array :",frutas[1]);
 
-
+// Recorrer el array frutas 
+for (let fruta of frutas){
+    console.log("Elementos del array frutas : ",fruta);
+}
  /**
   * Objetos 
   * Objeto : Datos organizados en pares clave-valor 
@@ -16,4 +19,4 @@
     nombre : "Elias",
     edad : 22
  }
- console.log(persona.edad);
+ console.log("Mi objeto Persona :",persona.nombre);
