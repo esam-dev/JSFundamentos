ee222289bfec29fdbab7b08a2570b5de8eacfc3f
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-11 12:48:52 -0300

    Practica conceptos de javascript

---
+++ AJFS-Fundamentos/EntradaUser/Leer_entradauser.js
@@ -0,0 +1,7 @@
+
+// Leer entrada del usuario y almacenar en una variable
+// Instalar paquete de nodejs prompt-sync
+
+const prompt = require ('prompt-sync')();
+infoLeida = prompt("Escribe algo: ");
+console.log("Has escrito : ", infoLeida);
